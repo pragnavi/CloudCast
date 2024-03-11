f0d1723a20779c8f10cffbbc813629503af0c456
pragnavi <pragnavi1398@gmail.com>
2024-03-10 23:35:31 -0400

    Created a View Controller

---
+++ CloudCast/ForecastViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ForecastViewController.swift
+//  CloudCast
+//
+//  Created by Pragnavi Ravuluri Sai Durga on 3/10/24.
+//
+
+import UIKit
+
+class ForecastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
