00b5bb31346ddd205a4a13a481331ca614267866
pragnavi <pragnavi1398@gmail.com>
2024-03-11 00:50:09 -0400

    Added mock data

--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -13,8 +13,18 @@ class ForecastViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let fakeData = WeatherForecast(temperature: 25.0, date: Date(), weatherCode: .partlyCloudy)
+        configure(with: fakeData)
     }
 
+    private func configure(with forecast: WeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)°F"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: forecast.date)
+      }
     
     @IBOutlet weak var locationLabel: UILabel!
     
